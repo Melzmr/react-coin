c7ad67ce6048cbf0817e86b2852a1b86f8f820df
rmartynenko <rmartynenko@mobiledimension.ru>
2018-04-23 16:53:25 +0300

    Adds README

---
+++ README.md
@@ -0,0 +1,7 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+Feel free to try out this [application](https://melz-react-coin.herokuapp.com/).
+
+A simple application that allows you to monitor information about nearly all cryptocurrencies in real time.
+
+This project was done to consolidate the knowledge gained.
--- src/components/list/Table.js
+++ src/components/list/Table.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { renderChangePercent } from '../../helpers';
-import PropTypes from 'prop-types';
-import './Table.css';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { renderChangePercent } from "../../helpers";
+import PropTypes from "prop-types";
+import "./Table.css";
 
-const Table = (props) => {
-  const { currencies, history,  } = props;
-  return  (
+const Table = props => {
+  const { currencies, history } = props;
+  return (
     <div className="Table-container">
       <table className="Table">
         <thead className="Table-head">
@@ -18,10 +18,10 @@ const Table = (props) => {
           </tr>
         </thead>
         <tbody className="Table-body">
-          {currencies.map((currency) => (
-            <tr 
-            key={currency.id}
-            onClick={() => history.push(`/currency/${currency.id}`)}
+          {currencies.map(currency => (
+            <tr
+              key={currency.id}
+              onClick={() => history.push(`/currency/${currency.id}`)}
             >
               <td>
                 <span className="Table-rank">{currency.rank}</span>
@@ -35,20 +35,18 @@ const Table = (props) => {
                 <span className="Table-dollar">$</span>
                 {currency.marketCap}
               </td>
-              <td>
-                {renderChangePercent(currency.percentChange24h)}
-              </td>
+              <td>{renderChangePercent(currency.percentChange24h)}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-    )
-}
+  );
+};
 
 Table.propTypes = {
   currencies: PropTypes.array.isRequired,
-  history: PropTypes.object.isRequired,
-}
+  history: PropTypes.object.isRequired
+};
 
 export default withRouter(Table);
